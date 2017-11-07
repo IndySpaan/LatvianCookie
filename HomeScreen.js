883e0c65f668242fe66af02211a491cae9c859a8
IndySpaan <indyspaan2@gmail.com>
2017-11-07 15:07:38 +0100

    Added button to homepage

--- HomeScreen.js
+++ HomeScreen.js
@@ -2,19 +2,29 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity,
+  ScrollView
 } from 'react-native';
 import { Actions } from 'react-native-router-flux'; // New code
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text
-        style={styles.welcome}
-        onPress={() => Actions.routeScreen()}
-      >
-        Home screen
-      </Text>
+      <View style={styles.containerStandard}>
+
+      </View>
+      <View style={styles.row}>
+        <TouchableOpacity onPress={() => Actions.routeScreen()}>
+          <View style={styles.button}>
+            <Text
+              style={styles.buttonText}
+            >
+              Generate a new route!
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -22,15 +32,32 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#fff',
   },
-  welcome: {
-    fontSize: 20,
+  containerStandard: {
+    flex: 1,
+    backgroundColor: '#fff',
+    flexDirection: 'row'
+  },
+  row: {
+    padding: 20,
+    backgroundColor: '#253a4b',
+  },
+  headerArea: {
+    backgroundColor: '#fff',
+  },
+  button: {
+    backgroundColor: '#f33b5f',
+    padding: 15,
+    borderRadius: 100,
+    elevation: 3,
+    marginLeft: 20,
+    marginRight: 20
+  },
+  buttonText: {
+    fontSize: 17,
     textAlign: 'center',
-    margin: 10,
-    color: '#1c1c1c',
+    color: '#fff',
   },
 });
 
