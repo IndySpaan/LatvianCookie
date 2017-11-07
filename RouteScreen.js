0d2ac69a490ae72f8da875ca13ac68ada6a44d01
Unknown <a.vesnenok@student.fontys.nl>
2017-11-07 15:53:58 +0100

    RouteScreen input field addition

--- RouteScreen.js
+++ RouteScreen.js
@@ -2,28 +2,38 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput
 } from 'react-native';
 import { Actions } from 'react-native-router-flux'; // New code
+import { TextField } from 'react-native-material-textfield';
 
-const RouteScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text
-        style={styles.welcome}
-      >
-        Route Screen
-      </Text>
-    </View>
-  );
+
+class RouteScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: '',};
+  }
+
+  render() {
+    let {text} = this.state;
+    return (
+      <View style={styles.container}>
+        <TextInput
+          value={text}
+          placeholder={'Input the distance to run.'}
+          onChangeText={(text) => this.setState({text})}
+          style={styles.input}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#e7e7e7',
+    backgroundColor: '#3498db',
   },
   welcome: {
     fontSize: 20,
@@ -31,6 +41,16 @@ const styles = StyleSheet.create({
     margin: 10,
     color: '#1c1c1c',
   },
+  input: {
+    height: 50,
+    backgroundColor: 'rgba(255,255,255,0.7)',
+    fontSize: 30,
+    color: '#FFF',
+    marginHorizontal: 20,
+    marginTop: 10
+
+
+  }
 });
 
 export default RouteScreen;
